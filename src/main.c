5eccf7be5cf0be915cfebbece74789bc289ab624
owenhochwald <owenhochwald@gmail.com>
2025-12-11 23:48:56 -0800

    implement command loop in main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <_stdlib.h>
 
+
+char * osh_read_line(void);
+
+char ** osh_split_line(char * line);
+
+int osh_execute(char ** args);
+
+// Read commands, parse into program, execute commands
+void osh_loop(void) {
+    char * line;
+    char ** args;
+    int status;
+
+    do {
+        printf("> ");
+        line = osh_read_line();
+        args = osh_split_line(line);
+        status = osh_execute(args);
+
+        free(line);
+        free(args);
+    } while (status);
+}
+
+// LIFETIME:
+// 1. Init, 2. Interpret, 3. Terminate
 int main(void) {
+    // Init
+
+    // Interpret. Run command loop
     printf("Hello, World!\n");
-    return 0;
+    osh_loop();
+
+    // Terminate
+
+
+    return EXIT_SUCCESS;
 }
